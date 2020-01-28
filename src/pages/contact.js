1fe6342362eea4c3747502d928bb3b3b0610e623
Brice Christian <bricechristian93@gmail.com>
2020-01-28 17:18:31 -0500

    Updated contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,14 +2,32 @@ import React from "react";
 import { Link, graphql } from "gatsby";
 import Img from "gatsby-image";
 import Layout from "../components/layout";
+import parse from "html-react-parser";
 
 import "../styles/contact.css";
 
-const ContactPage = ({ data }) => (
-  <Layout>
-    <h1>Contact</h1>
-  </Layout>
-);
+const ContactPage = ({ data }) => {
+  const contact = data.datoCmsContactPage;
+  console.log(contact);
+  return (
+    <Layout>
+      <div id="contact">
+        <div className="main flex wrapper skinny">
+          <div className="three-fourths text">
+            {parse(contact.headline)}
+            <div className="flex bottom inner space-between">
+              <div className="one-half">{parse(contact.leftBlockText)}</div>
+              <div className="one-half">{parse(contact.rightBlockText)}</div>
+            </div>
+          </div>
+          <div className="one-fourth">
+            <Img fluid={contact.image.fluid} />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
 export default ContactPage;
 
@@ -21,6 +39,14 @@ export const query = graphql`
       }
       title
       slug
+      headline
+      image {
+        fluid(maxWidth: 550, imgixParams: { fm: "jpg", auto: "compress" }) {
+          ...GatsbyDatoCmsFluid
+        }
+      }
+      leftBlockText
+      rightBlockText
     }
   }
 `;

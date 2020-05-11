029bd719440b81fd612464526abc90bda4cd6f34
Brice Christian <bricechristian93@gmail.com>
2020-05-11 15:15:53 -0400

    removed gatsby plugin manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,24 +30,6 @@ module.exports = {
         trackingId: "UA-111080132-1",
       },
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: "Outline | Branding | Web Design | Charleston, SC",
-        short_name: "Outline",
-        start_url: "/",
-        background_color: "#F5F5F1",
-        theme_color: "#000000",
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: "standalone",
-        icon: "static/icon.png", // This path is relative to the root of the site.
-        // An optional attribute which provides support for CORS check.
-        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
-        // Any invalid keyword or empty string defaults to `anonymous`
-        crossOrigin: `use-credentials`,
-      },
-    },
     "gatsby-plugin-offline",
   ],
 };
